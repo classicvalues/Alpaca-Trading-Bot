2c11807078ce556ad2f0f4d51a807eda39d75016
Classic Values <classicvaluesllc@gmail.com>
2024-10-07 15:14:29 -0700

    COMMIT
    Create Trades.py

---
+++ Data/Day 1/Trades.py
@@ -0,0 +1,169 @@
+---------------------------------------------
+##OPEN##
+---------------------------------------------
+BUY #1
+---------------------------------------------
+GOOGL
+Market Buy 2 Shares
+Order ID
+9d0ab659-7432-49ff-88da-0a25fa44c1bf
+Status
+filled
+Symbol
+GOOGL
+Side
+buy
+Order Type
+market
+Quantity
+2
+Time In Force
+gtc
+Extended Hours
+No
+Limit Price
+-
+Stop Price
+-
+Created At
+2024-10-06T23:17:25.282262825Z
+Submitted At
+2024-10-07T08:00:06.449316286Z
+Lifecycle of Status
+see history
+Created At2024-10-06T23:17:25.282262825Z
+Submitted At2024-10-07T08:00:06.449316286Z
+Filled At2024-10-07T13:30:09.545624004Z
+Updated At2024-10-07T13:30:09.547922867Z
+Order Linkages
+see details
+Replaces
+Replaced By
+-
+-
+---------------------------------------------
+BUY #2
+---------------------------------------------
+GOOGL
+Market Buy 598 Shares
+Order ID
+68d76bfa-45db-4e33-8f3f-5cc9cf73a666
+Status
+filled
+Symbol
+GOOGL
+Side
+buy
+Order Type
+market
+Quantity
+598
+Time In Force
+gtc
+Extended Hours
+No
+Limit Price
+-
+Stop Price
+-
+Created At
+2024-10-06T23:08:27.046253233Z
+Submitted At
+2024-10-07T08:00:06.452632276Z
+Lifecycle of Status
+see history
+Created At2024-10-06T23:08:27.046253233Z
+Submitted At2024-10-07T08:00:06.452632276Z
+Filled At2024-10-07T13:30:24.58492707Z
+Updated At2024-10-07T13:30:24.58629984Z
+Order Linkages
+see details
+Replaces
+Replaced By
+-
+-
+---------------------------------------------
+BUY #3
+---------------------------------------------
+GOOGL
+Market Buy 598 Shares
+Order ID
+28edd09a-d694-4718-a046-a92febbc64a0
+Status
+filled
+Symbol
+GOOGL
+Side
+buy
+Order Type
+market
+Quantity
+598
+Time In Force
+gtc
+Extended Hours
+No
+Limit Price
+-
+Stop Price
+-
+Created At
+2024-10-06T22:54:46.744323345Z
+Submitted At
+2024-10-07T08:00:06.455938516Z
+Lifecycle of Status
+see history
+Created At2024-10-06T22:54:46.744323345Z
+Submitted At2024-10-07T08:00:06.455938516Z
+Filled At2024-10-07T13:30:16.514268246Z
+Updated At2024-10-07T13:30:16.515986948Z
+Order Linkages
+see details
+Replaces
+Replaced By
+-
+-
+---------------------------------------------
+SELL #1
+---------------------------------------------
+GOOGL
+Market Sell 1198 Shares
+Order ID
+f84da41f-3e8e-42f2-91a6-883e43680eee
+Status
+filled
+Symbol
+GOOGL
+Side
+sell
+Order Type
+market
+Quantity
+1198
+Time In Force
+gtc
+Extended Hours
+No
+Limit Price
+-
+Stop Price
+-
+Created At
+2024-10-07T13:30:35.430820883Z
+Submitted At
+2024-10-07T13:30:35.434807485Z
+Lifecycle of Status
+see history
+Created At2024-10-07T13:30:35.430820883Z
+Submitted At2024-10-07T13:30:35.434807485Z
+Filled At2024-10-07T13:30:35.840777236Z
+Updated At2024-10-07T13:30:35.842919194Z
+Order Linkages
+see details
+Replaces
+Replaced By
+-
+-
+---------------------------------------------
+##CLOSE##
+---------------------------------------------
